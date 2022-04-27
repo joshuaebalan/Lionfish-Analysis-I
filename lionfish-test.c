2c3c42ba705d37bd8ac502c8084a72d87997b6b4
Joshua Balan <jbalan@purdue.edu>
2022-04-27 17:23:17 -0400

    added defined functions in .h to the lionfish-test.c file.

--- lionfish-test.c
+++ lionfish-test.c
@@ -5,5 +5,34 @@
 #include <stdlib.h>
 
 int main() {
-  return 0;
+  return OK;
+}
+
+int relate_time(lionfish_t *a, lionfish_t *b) {
+  if (a->time_caught->id > b->time_caught->id) {
+    return GREATER;
+  }
+  else if (a->time_caught->id < b->time_caught->id) {
+    return LESSER;
+  }
+  else if (a->time_caught->id == b->time_caught->id) {
+    return EQUAL;
+  }
+  else {
+    return UH-OH;
+  }
+}
+
+int compile_time(catch_t *catch) {
+  float tot = 0.0;
+  tot = tot + (float)catch->year;
+  tot = tot + ((float)catch->month / 12);
+  if (catch->year % 4 == 0) {
+    tot = tot + ((float)catch->day / 366);
+  }
+  else {
+    tot = tot + ((float)catch->day / 365);
+  }
+  catch->id = tot;
+  return OK;
 }
