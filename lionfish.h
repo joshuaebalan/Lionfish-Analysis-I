66d2c90cab1a660f74edb2d8787a79403f6fe52c
Joshua Balan <jbalan@purdue.edu>
2022-05-03 13:48:55 -0400

    Revert "added time sorting capabilites"
    This reverts commit 7ae37ed768df7570e1cbfad8bc27357930cc22df.

--- lionfish.h
+++ lionfish.h
@@ -7,26 +7,17 @@
 #define LATER   (2) //See 'relate_time()'
 #define EQUAL	(3) //See 'relate_time()'
 #define EARLIER (4) //See 'relate_time()'
-#define MALE (101)
-#define FEMALE (102)
-#define UNIDENTIFIED (103)
-
-/* mode sepcifiers for group_time() */
-#define YEAR (2000) //search for fish in specified year
-#define MONTH (2001) //search for fish in specified month
-#define DAY (2002) //search for fish in specified day
 
 /* error codes */
+
 #define UH_OH   (-1) //if a point in the program is reached that shouldn't be
 #define BAD_TBL_READ (-2) //error in reading a text(i.e. CSV) file
 #define BAD_TBL_WRITE (-3) //error in writing to a text file in tables form.
 #define BAD_BIN_READ (-4) //error in reading from a binary file
 #define BAD_BIN_WRITE (-5) //error in writing to a binary file
-#define NO_FISH_FOUND (-6) //if a search function can't find anything
 
-/* applicable structures */
 typedef struct lionfish {
-  int sex;
+  enum {MALE, FEMALE, UNIDENTIFIED} sex;
   char *diet;
   float length;
   float mass;
@@ -44,7 +35,7 @@ int main();
 int relate_time(catch_t *, catch_t *);
 int enumerate_sex(char *);
 catch_t *read_from_writing(char *);
-int group_time(int, unsigned int, int, lionfish_t**, lionfish_t***);
+
 /* functions inside lionfish_IO.c */
 
 lionfish_t **read_table_file(char *);
